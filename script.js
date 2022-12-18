de35cd2adfa384a8a02e34b0508cce56a6f34c73
dsh5312 <119956650+dsh5312@users.noreply.github.com>
2022-12-19 00:59:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let burger = document.getElementsByClassName("burger")[0]
+let links = document.getElementsByClassName("links")[0]
+burger.addEventListener('click',()=>{
+    links.classList.toggle('active')
+})
